5fc236bc144b3aa8fcc46b6c42cfa1de4ef652f9
Ricael <ricaelmenezes@gmail.com>
2026-04-21 18:06:44 -0300

    feat: manually create SvelteKit project structure

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  let message = 'Carregando...';
+
+  onMount(async () => {
+    try {
+      const response = await fetch('http://localhost:8000');
+      const data = await response.json();
+      message = data.message;
+    } catch (error) {
+      message = 'Erro ao conectar com a API.';
+    }
+  });
+</script>
+
+<h1>Bem-vindo ao Griô</h1>
+<p>Status da API: {message}</p>
