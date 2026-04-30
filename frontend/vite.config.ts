78ddc3a6b23037b5181e9117db79e6300f8aef7f
Ricael Menezes Durand <ricaelmenezes@gmail.com>
2026-04-30 15:46:29 -0300

    chore(Docker): configurar watch mode para backend e frontend no docker-compose e HMR polling no vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		watch: {
+			usePolling: true, // Ativa polling para HMR funcionar dentro do Docker no Windows
+			interval: 100
+		}
+	}
 });
